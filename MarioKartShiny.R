dbcb31bfaf72898cb33e1e5fbd33d412349da38f
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-03-08 20:23:13 -0500

    Adding plots to shiny

--- MarioKartShiny.R
+++ MarioKartShiny.R
@@ -23,36 +23,133 @@ ui <- fluidPage(theme = shinytheme("slate"),
                          tabPanel("Characters",
                                   sidebarLayout(
                                     sidebarPanel(
-                                      selectInput("Character.Selector", h3("Character Select"),
+                                      selectInput("Character.Selector1", h3("Character Select 1"),
                                                   choices = Choices.c, 
-                                                  selected = "Baby Daisy")),
+                                                  selected = "Baby Daisy"),
+                                      selectInput("Character.Selector2", h3("Character Select 2"),
+                                                choices = "")),
                                     mainPanel(plotlyOutput("Character.Plot"))
       )
     ), 
-    tabPanel("Karts",
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput("Kart.Selector", h3("Kart Select"),
-                             choices = Choices.k, 
-                             selected = "Blue Flacon")),
-               mainPanel(plotlyOutput("Kart.Plot"))
+                        tabPanel("Karts",
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   selectInput("Kart.Selector.Type", h3("Select Kart Type"),
+                                               choices = c("Light", "Medium", "Heavy"), 
+                                               selected = "Light"), 
+                                   selectInput("Kart.Selector1", h3("Kart Select 1"),
+                                               choices = Choices.k, 
+                                               selected = "Blue Flacon"),
+                                   selectInput("Kart.Selector2", h3("Kart Select 2"),
+                                               choices = "")),
+                                 mainPanel(plotlyOutput("Kart.Plot"))
       )
     ),
-    tabPanel("Bikes",
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput("Bike.Selector", h3("Bike Select"),
-                             choices = Choices.k, 
-                             selected = "Bit Bike")),
-               mainPanel(plotlyOutput("Bike.Plot"))
+                        tabPanel("Bikes",
+                                 sidebarLayout(
+                                   sidebarPanel(
+                                     selectInput("Bike.Selector.Type", h3("Select Bike Type"),
+                                                 choices = c("Light", "Medium", "Heavy"), 
+                                                 selected = "Light"),
+                                     selectInput("Bike.Selector1", h3("Bike Select 1"),
+                                                 choices = Choices.b, 
+                                                 selected = "Bit Bike"),
+                                     selectInput("Bike.Selector2", h3("Bike Select 2"),
+                                                 choices = "")),
+                                   mainPanel(plotlyOutput("Bike.Plot"))
       )
     )
   )
 )
 
-
+##### Server #####
 server <- function(input, output, session) {
+#### Selector reactivity #####
+  observe({
+    xvar = input$Character.Selector1
+    updateSelectInput(session, "Character.Selector2", choices = Choices.c[-which(Choices.c == input$Character.Selector1)])
+  }) 
   
+  observe({
+    xvar = input$Kart.Selector.Type
+    updateSelectInput(session, "Kart.Selector1", choices = karts$Kart[which(karts$Class == input$Kart.Selector.Type)])
+  })   
+  
+  observe({
+    xvar = input$Kart.Selector1
+    updateSelectInput(session, "Kart.Selector2", choices = karts$Kart[which(karts$Class == input$Kart.Selector.Type &
+                                                                              karts$Kart != input$Kart.Selector1)])
+  }) 
+  
+  observe({
+    xvar = input$Bike.Selector.Type
+    updateSelectInput(session, "Bike.Selector1", choices = bikes$Bike[which(bikes$Class == input$Bike.Selector.Type)])
+  }) 
+  
+  observe({
+    xvar = input$Bike.Selector1
+    updateSelectInput(session, "Bike.Selector2", choices = bikes$Bike[which(bikes$Class == input$Bike.Selector.Type &
+                                                                              bikes$Bike != input$Bike.Selector1)])
+  }) 
+  
+#### Plots ####
+  
+  output$Character.Plot <- renderPlotly({
+    
+    xvar <- input$Character.Selector1
+    yvar <- input$Character.Selector2
+    
+    characters %>% 
+      pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo), 
+                   names_to = "Attribute", values_to = "Score") %>% 
+      filter(Character == xvar | Character == yvar) %>% 
+      ggplot(aes(x=Attribute, y=ifelse(Score == 0, -.025, Score))) +
+      geom_bar(aes(fill = Character), stat = "identity", position = "dodge") +
+      coord_flip() +
+      theme_minimal() + 
+      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", colour="black", vjust=-1)) +
+      labs(title = "Character Evaluator", 
+           y = 'Rating') +
+      scale_fill_brewer(palette="Paired")
+  })
+  
+  output$Kart.Plot <- renderPlotly({
+    
+    xvar <- input$Kart.Selector1
+    yvar <- input$Kart.Selector2
+    
+    karts %>% 
+      pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo), 
+                   names_to = "Attribute", values_to = "Score") %>% 
+      filter(Kart == xvar | Kart == yvar) %>% 
+      ggplot(aes(x=Attribute, y=ifelse(Score == 0, -.025, Score))) +
+      geom_bar(aes(fill = Kart), stat = "identity", position = "dodge") +
+      coord_flip() +
+      theme_minimal() + 
+      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", colour="black", vjust=-1)) +
+      labs(title = "Kart Evaluator", 
+           y = 'Rating') +
+      scale_fill_brewer(palette="Paired")
+  })
+  
+  output$Bike.Plot <- renderPlotly({
+    
+    xvar <- input$Bike.Selector1
+    yvar <- input$Bike.Selector2
+    
+    bikes %>% 
+      pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo), 
+                   names_to = "Attribute", values_to = "Score") %>% 
+      filter(Bike == xvar | Bike == yvar) %>% 
+      ggplot(aes(x=Attribute, y=ifelse(Score == 0, -.025, Score))) +
+      geom_bar(aes(fill = Bike), stat = "identity", position = "dodge") +
+      coord_flip() +
+      theme_minimal() + 
+      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", colour="black", vjust=-1)) +
+      labs(title = "Bike Evaluator", 
+           y = 'Rating') +
+      scale_fill_brewer(palette="Paired")
+  })
 }
 
 shinyApp(ui = ui, server = server)
