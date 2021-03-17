34c49eca4f6502af41c1827abdd146619008e1ab
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-03-16 21:51:45 -0400

    Did a lot, reworked the whole thing into one page

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM rocker/verse
 MAINTAINER Matt Johnson <Johnson.Matt1818@gmail.com>
 RUN R -e "install.packages('shiny')"
 RUN R -e "install.packages('shinythemes')"
-RUN R -e "install.packages('plotly')"
--- MarioKartShiny.R
+++ MarioKartShiny.R
@@ -2,7 +2,6 @@
 library(shiny)
 library(shinythemes)
 library(tidyverse)
-library(plotly)
 
 ##args <- commandArgs(trailingOnly = T)
 ##port <- as.numeric(args[[1]])
@@ -20,55 +19,37 @@ Choices.b <- sort(bikes$Bike)
 ##### UI #####
 ui <- fluidPage(theme = shinytheme("slate"),
               navbarPage("Mario Kart Wii",
-                         tabPanel("Characters",
-                                  sidebarLayout(
-                                    sidebarPanel(
-                                      selectInput("Character.Selector1", h3("Character Select 1"),
-                                                  choices = Choices.c, 
-                                                  selected = "Baby Daisy"),
-                                      selectInput("Character.Selector2", h3("Character Select 2"),
-                                                choices = "")),
-                                    mainPanel(plotlyOutput("Character.Plot"))
-      )
-    ), 
-                        tabPanel("Karts",
-                               sidebarLayout(
-                                 sidebarPanel(
-                                   radioButtons("Kart.Selector.Type1", h3("Select Kart Type 1"),
-                                               choices = c("Light", "Medium", "Heavy"), 
-                                               selected = "Light", 
-                                               inline = T), 
-                                   selectInput("Kart.Selector1", h3("Kart Select 1"),
-                                               choices = Choices.k, 
-                                               selected = "Blue Flacon"),
-                                   radioButtons("Kart.Selector.Type2", h3("Select Kart Type 2"),
-                                                choices = c("Light", "Medium", "Heavy"), 
-                                                selected = "Light", 
-                                                inline = T),
-                                   selectInput("Kart.Selector2", h3("Kart Select 2"),
-                                               choices = "")),
-                                 mainPanel(plotlyOutput("Kart.Plot"))
-      )
-    ),
-                        tabPanel("Bikes",
+                        tabPanel("Evaluation Tool", 
                                  sidebarLayout(
                                    sidebarPanel(
-                                     radioButtons("Bike.Selector.Type1", h3("Select Bike Type 1"),
-                                                 choices = c("Light", "Medium", "Heavy"), 
-                                                 selected = "Light", 
-                                                 inline = T),
-                                     selectInput("Bike.Selector1", h3("Bike Select 1"),
-                                                 choices = Choices.b, 
-                                                 selected = "Bit Bike"),
-                                     radioButtons("Bike.Selector.Type2", h3("Select Bike Type 2"),
+                                     radioButtons("Character.1.Type", h3("Select Character 1 Type"), 
                                                   choices = c("Light", "Medium", "Heavy"), 
                                                   selected = "Light", 
-                                                  inline = T),
-                                     selectInput("Bike.Selector2", h3("Bike Select 2"),
-                                                 choices = "")),
-                                   mainPanel(plotlyOutput("Bike.Plot"))
-      )
-    )
+                                                  inline = T), 
+                                     selectInput("Character.1.Selector", h3("Select Character 1"),
+                                                 choices = ""), 
+                                     radioButtons("Vehicle.1.Type.Selector", h3("Select Vehicle 1 Type"), 
+                                                  choices = c("Bike", "Kart"), 
+                                                  selected = "Bike"),
+                                     selectInput("Vehicle.1.Selector", h3("Select Vehicle 1"), 
+                                                 choices = ""),
+                                     radioButtons("Character.2.Type", h3("Select Character 2 Type"), 
+                                                  choices = c("Light", "Medium", "Heavy"), 
+                                                  selected = "Light", 
+                                                  inline = T), 
+                                     selectInput("Character.2.Selector", h3("Select Character 2"),
+                                                 choices = ""), 
+                                     radioButtons("Vehicle.2.Type.Selector", h3("Select Vehicle 2 Type"), 
+                                                  choices = c("Bike", "Kart"), 
+                                                  selected = "Bike"),
+                                     selectInput("Vehicle.2.Selector", h3("Select Vehicle 2"), 
+                                                 choices = "")
+                                   ),
+                                   mainPanel(plotOutput("Eval.Plot")), 
+                                   position = "left"
+                                 )
+                                 
+                            )
   )
 )
 
@@ -78,105 +59,123 @@ server <- function(input, output, session) {
   
   ##### Characters #####
   observe({
-    xvar = input$Character.Selector1
-    updateSelectInput(session, "Character.Selector2", choices = Choices.c[-which(Choices.c == input$Character.Selector1)])
+    xvar = input$Character.1.Type
+    updateSelectInput(session, "Character.1.Selector", choices = characters$Character[which(characters$Class == input$Character.1.Type)])
+  })  
+  
+  observe({
+    xvar = input$Character.2.Type
+    updateSelectInput(session, "Character.2.Selector", choices = characters$Character[which(characters$Class == input$Character.2.Type)])
   }) 
   
+  ##### Vehicles #####
   observe({
-    xvar = input$Kart.Selector.Type1
-    updateSelectInput(session, "Kart.Selector1", choices = karts$Kart[which(karts$Class == input$Kart.Selector.Type1)])
-  })   
+    if (input$Vehicle.1.Type.Selector == "Bike") {
+    xvar = input$Vehicle.1.Type.Selector
+    updateSelectInput(session, "Vehicle.1.Selector",
+                      choices = bikes$Bike[which(bikes$Class == input$Character.1.Type)])
+    } else {
+      xvar = input$Vehicle.1.Type.Selector
+      updateSelectInput(session, "Vehicle.1.Selector",
+                        choices = karts$Kart[which(karts$Class == input$Character.1.Type)])
+    }
+  })  
   
-  ##### Karts #####
   observe({
-    xvar = input$Kart.Selector.Type2
-    updateSelectInput(session, "Kart.Selector2", choices = karts$Kart[which(karts$Class == input$Kart.Selector.Type2)])
+   if (input$Character.1.Selector == input$Character.2.Selector) {
+     if (input$Vehicle.2.Type.Selector == "Bike") {
+      xvar = input$Vehicle.2.Type.Selector
+      updateSelectInput(session, "Vehicle.2.Selector",
+                        choices = bikes$Bike[which(bikes$Class == input$Character.2.Type &
+                                                     bikes$Bike != input$Vehicle.1.Selector)])
+    } else {
+      xvar = input$Vehicle.2.Type.Selector
+      updateSelectInput(session, "Vehicle.2.Selector",
+                        choices = karts$Kart[which(karts$Class == input$Character.2.Type &
+                                                     karts$Kart != input$Vehicle.1.Selector)])
+    }
+   } else {
+     if (input$Vehicle.2.Type.Selector == "Bike") {
+       xvar = input$Vehicle.2.Type.Selector
+       updateSelectInput(session, "Vehicle.2.Selector",
+                         choices = bikes$Bike[which(bikes$Class == input$Character.2.Type)])
+     } else {
+       xvar = input$Vehicle.2.Type.Selector
+       updateSelectInput(session, "Vehicle.2.Selector",
+                         choices = karts$Kart[which(karts$Class == input$Character.2.Type)])
+     }
+   }
+    
   })
   
-  observe({
-    if (input$Kart.Selector.Type1 == input$Kart.Selector.Type2) {
-    xvar = input$Kart.Selector1
-    updateSelectInput(session, "Kart.Selector2", choices = karts$Kart[which(karts$Class == input$Kart.Selector.Type1 &
-                                                                              karts$Kart != input$Kart.Selector1)])
-    }
-  }) 
+#### Plot ####
   
-  ##### Bikes #####
-  observe({
-    xvar = input$Bike.Selector.Type1
-    updateSelectInput(session, "Bike.Selector1", choices = bikes$Bike[which(bikes$Class == input$Bike.Selector.Type1)])
-  }) 
   
-  observe({
-    xvar = input$Bike.Selector.Type2
-    updateSelectInput(session, "Bike.Selector2", choices = bikes$Bike[which(bikes$Class == input$Bike.Selector.Type2)])
-  }) 
+  ##### Build DF #####
   
-  observe({
-    if (input$Bike.Selector.Type1 == input$Bike.Selector.Type2) {
-    xvar = input$Bike.Selector1
-    updateSelectInput(session, "Bike.Selector2", choices = bikes$Bike[which(bikes$Class == input$Bike.Selector.Type1 &
-                                                                              bikes$Bike != input$Bike.Selector1)])
-    }
-  }) 
-  
-#### Plots ####
-  
-  output$Character.Plot <- renderPlotly({
-    
-    xvar <- input$Character.Selector1
-    yvar <- input$Character.Selector2
-    
-    characters %>% 
-      pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo), 
-                   names_to = "Attribute", values_to = "Score") %>% 
-      filter(Character == xvar | Character == yvar) %>% 
-      ggplot(aes(x=Attribute, y=ifelse(Score == 0, -.025, Score))) +
+  pivot.bikes <- bikes %>%
+    pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo),
+                 names_to = "Attribute", values_to = "Vehicle.Score") %>%
+    rename("Vehicle" = Bike)
+
+  pivot.karts <- karts %>%
+    pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo),
+                 names_to = "Attribute", values_to = "Vehicle.Score") %>%
+    rename("Vehicle" = Kart)
+
+  pivot.vehicle <- rbind(pivot.bikes, pivot.karts)
+
+  pivot.characters <- characters %>%
+    pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo),
+                 names_to = "Attribute", values_to = "Score")
+
+  DF <- left_join(pivot.characters, pivot.vehicle)
+  Pivot.DF <- DF %>%
+    mutate(Final.Score = Score + Vehicle.Score)
+
+     Plot.func <-  reactive({
+       ##### Define Inputs #####
+       C1.Type <- input$Character.1.Type
+       C1.Name <- input$Character.1.Selector
+       C1.V <- input$Vehicle.1.Selector
+       
+       C2.Type <- input$Character.2.Type
+       C2.Name <- input$Character.2.Selector
+       C2.V <- input$Vehicle.2.Selector 
+      
+  if(C1.Name != C2.Name)  {
+    Pivot.DF %>%
+      filter(Class == C1.Type | Class == C2.Type) %>%
+      filter(Character == C1.Name | Character == C2.Name) %>%
+      filter(Vehicle == C1.V | Vehicle == C2.V) %>%
+      ggplot(aes(x=Attribute, y=Final.Score)) +
       geom_bar(aes(fill = Character), stat = "identity", position = "dodge") +
       coord_flip() +
-      theme_minimal() + 
-      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", colour="black", vjust=-1)) +
-      labs(title = "Character Evaluator", 
+      theme_minimal() +
+      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", color="black", vjust=-1), 
+            axis.text = element_text(color = "black")) +
+      labs(title = "Evaluation",
            y = 'Rating') +
       scale_fill_brewer(palette="Paired")
+  } else {
+    Pivot.DF %>%
+      filter(Class == C1.Type | Class == C2.Type) %>%
+      filter(Character == C1.Name | Character == C2.Name) %>%
+      filter(Vehicle == C1.V | Vehicle == C2.V) %>%
+      ggplot(aes(x=Attribute, y=Final.Score)) +
+      geom_bar(aes(fill = Vehicle), stat = "identity", position = "dodge") +
+      coord_flip() +
+      theme_minimal() +
+      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", color="black", vjust=-1), 
+            axis.text = element_text(color = "black")) +
+      labs(title = "Evaluation",
+           y = 'Rating') +
+      scale_fill_brewer(palette="Paired")
+  }
   })
   
-  output$Kart.Plot <- renderPlotly({
-    
-    xvar <- input$Kart.Selector1
-    yvar <- input$Kart.Selector2
-    
-    karts %>% 
-      pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo), 
-                   names_to = "Attribute", values_to = "Score") %>% 
-      filter(Kart == xvar | Kart == yvar) %>% 
-      ggplot(aes(x=Attribute, y=ifelse(Score == 0, -.025, Score))) +
-      geom_bar(aes(fill = Kart), stat = "identity", position = "dodge") +
-      coord_flip() +
-      theme_minimal() + 
-      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", colour="black", vjust=-1)) +
-      labs(title = "Kart Evaluator", 
-           y = 'Rating') +
-      scale_fill_brewer(palette="Paired")
-  })
-  
-  output$Bike.Plot <- renderPlotly({
-    
-    xvar <- input$Bike.Selector1
-    yvar <- input$Bike.Selector2
-    
-    bikes %>% 
-      pivot_longer(c(Speed, Weight, Acceleration, Handling, Drift, Off.Road, Mini.Turbo), 
-                   names_to = "Attribute", values_to = "Score") %>% 
-      filter(Bike == xvar | Bike == yvar) %>% 
-      ggplot(aes(x=Attribute, y=ifelse(Score == 0, -.025, Score))) +
-      geom_bar(aes(fill = Bike), stat = "identity", position = "dodge") +
-      coord_flip() +
-      theme_minimal() + 
-      theme(plot.title=element_text(size=18, hjust=0.5, face="bold", colour="black", vjust=-1)) +
-      labs(title = "Bike Evaluator", 
-           y = 'Rating') +
-      scale_fill_brewer(palette="Paired")
+  output$Eval.Plot <- renderPlot({
+    Plot.func()
   })
 }
 
